66deb71947fe0e3555d078587231562a3d4d4ffb
Sunilkumartechglobal <sunilktechglobal@gmail.com>
2026-03-02 10:34:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
-import RootLayout from  './components/layout/RootLayout';
+import RootLayout from './components/layout/RootLayout';
 import HomePage from './pages/HomePage';
 import FlightsPage from './pages/flights/FlightsPage';
 import BusesPage from './pages/buses/BusesPage';
@@ -12,56 +11,40 @@ import HouseboatsList from './components/HouseboatsList';
 import ScrollToTop from './components/ScrollToTop';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
-import AdminRegister from './pages/admin/AdminRegister';
-import OfferForm from './pages/admin/OfferForm';
 import DestinationFeed from './pages/DestinationFeed';
 import AboutPage from './pages/AboutPage';
 import InfoPage from './components/info';
 import DestinationDetails from './pages/DestinationDetail';
 import ContactPage from './pages/ContactPage';
 import DestinationsPage from './components/DestinationsPage';
-import CookieConsent from './components/CookieConsent';
 import CookieConsentWithUserData from './components/CookieConsentWithUserData';
 
-
-// add other pages as needed
-
 export default function App() {
-  return (
-    <CookiesProvider>
-    <BrowserRouter>
-    <ScrollToTop />
-      <Routes>
-        <Route element={<RootLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="flights" element={<FlightsPage />} />
-          <Route path="buses" element={<BusesPage />} />
-          <Route path="trains" element={<TrainsPage />} />
-          <Route path="houseboats" element={<HouseboatsList />} />
-          <Route path="/houseboats/:title" element={<HouseboatDetail />} />
-          <Route path="destinationfeed" element={<DestinationFeed />} />
-          <Route path="destination/:id" element={<DestinationDetails />} />
-          <Route path="/info" element={<InfoPage/>} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/destinations" element={<DestinationsPage />} />
-        </Route>
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-
-        <Route path="about" element={<AboutPage />} />
-
-          {/* Admin Routes (No Layout - standalone pages) */}
-          {/* <Route path="TP-admin/login" element={<AdminLogin />} />
-          <Route path="TP-admin/dashboard" element={<AdminDashboard />} />
-          <Route path="TP-admin/register" element={<AdminRegister />} />
-          <Route path="TP-admin/offers" element={<OfferForm />} /> */}
-
-
-          {/* 404 fallback */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-      <CookieConsentWithUserData />
-    </BrowserRouter>
-    </CookiesProvider>
-  );
+return (
+<CookiesProvider>
+<BrowserRouter>
+<ScrollToTop />
+<Routes>
+<Route element={<RootLayout />}>
+<Route index element={ <HomePage /> } />
+<Route path="flights" element={ <FlightsPage /> } />
+<Route path="buses" element={ <BusesPage /> } />
+<Route path="trains" element={ <TrainsPage /> } />
+<Route path="houseboats" element={ <HouseboatsList /> } />
+<Route path="houseboats/:id" element={ <HouseboatDetail /> } />
+<Route path="destinationfeed" element={ <DestinationFeed /> } />
+<Route path="destination/:id" element={ <DestinationDetails /> } />
+<Route path="info" element={ <InfoPage /> } />
+<Route path="contact" element={ <ContactPage /> } />
+<Route path="destinations" element={ <DestinationsPage /> } />
+</Route>
+<Route path="/admin/login" element={ <AdminLogin /> } />
+<Route path="/admin/dashboard" element={ <AdminDashboard /> } />
+<Route path="about" element={ <AboutPage /> } />
+<Route path="*" element={ <Navigate to="/" replace /> } />
+</Routes>
+<CookieConsentWithUserData />
+</BrowserRouter>
+</CookiesProvider>
+);
 }
